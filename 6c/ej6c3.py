c1465826f966d94fc9215391ae0e5ac205abd036
Lucas132005 <eguibo2@gmail.com>
2026-05-04 17:25:36 +0200

    Ejercicio 3 hecho

--- 6c/ej6c3.py
+++ 6c/ej6c3.py
@@ -85,25 +85,31 @@ Exemple:
          dtype='object')
 
 """
+
 import pandas as pd
 
 
 def read_parquet_file(path: str) -> pd.DataFrame:
-    #Write your code here
-    pass
+    #archivo = ruta[-1]
+    with open(path,"r"):
+        dataframe = pd.read_parquet(path)
+        return dataframe
 
 
 def calculate_amount_quanity(dataframe: pd.DataFrame):
-    #Write your code here
-    pass
+    
+    serie_amount=dataframe["price"]*dataframe["units_sold"]
+    dataframe["amount"] = serie_amount
+    
+    return dataframe
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# df_sales = read_parquet_file("files/sales_products_2020_08.parquet")
-# print(df_sales.columns)
-#
-# df_sales = calculate_amount_quanity(df_sales)
-# print(df_sales.columns)
-# print(df_sales[["price", "units_sold", "amount"]].head())
+df_sales = read_parquet_file("files/sales_products_2020_08.parquet")
+print(df_sales.columns)
+
+df_sales = calculate_amount_quanity(df_sales)
+print(df_sales.columns)
+print(df_sales[["price", "units_sold", "amount"]].head())
